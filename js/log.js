2123dece90c334f544ac6fd41c3825f6a290359d
IdleSouls <matteo_palombo@hotmail.it>
2025-08-27 15:34:48 +0200

    Update log.js

--- js/log.js
+++ js/log.js
@@ -1,11 +1,12 @@
 // Funzione per aggiungere nuovi log
 function updateLog(message) {
     const logContainer = document.getElementById('log');
-    
+    console.log('Aggiungo log: ', message); // Debug: mostra cosa viene aggiunto al log
+
     // Crea un nuovo elemento div per il log
     const newLog = document.createElement('div');
     newLog.textContent = message;
-    
+
     // Aggiungi il nuovo log all'inizio del contenitore
     logContainer.insertBefore(newLog, logContainer.firstChild);
 
