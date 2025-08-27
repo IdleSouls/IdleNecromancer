a88323bbfa1905eb3fd482d8c39775f469a75c0a
IdleSouls <matteo_palombo@hotmail.it>
2025-08-27 11:44:14 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,19 @@
+let resources = 0;
+let upgradeCost = 10;
+
+document.getElementById('gatherButton').addEventListener('click', () => {
+    resources++;
+    document.getElementById('resourceCount').textContent = resources;
+});
+
+document.getElementById('upgradeButton').addEventListener('click', () => {
+    if (resources >= upgradeCost) {
+        resources -= upgradeCost;
+        upgradeCost = Math.floor(upgradeCost * 1.5);
+        document.getElementById('resourceCount').textContent = resources;
+        alert('Upgrade acquistato!');
+    } else {
+        alert('Non hai abbastanza risorse!');
+    }
+});
 
